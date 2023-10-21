cbf60beb10c70ee3bf40facc650d91ba4018c4c4
kxding <91792104+kxding@users.noreply.github.com>
2023-10-21 13:31:05 +0800

    v1

---
+++ run.sh
@@ -0,0 +1,56 @@
+#！/bin/bash
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=0
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=2
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=3
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=4
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=7
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./animal"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-2
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./animal"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./animal"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-2
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./animal"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=0
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./animal"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=2
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./2k_pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=7
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./2k_pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./2k_pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-2
+
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./2k_pokemont"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=0
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./2k_pokemon"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=0
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=2
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=3
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=4
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=7
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-1
+
+accelerate launch main.py --concept_image_dir="./concept" --content_image_dir="./content"  --initializer_token="pokemon" --pretrained_model_name_or_path="/home/dingkaixin/improved/stable-diffusion-logo-fine-tuned" --cross_attention_injection_ratio=0.2 --self_attention_injection_ratio=0.9 --use_l1 --guidance_scale=-2
+
+
+
+
+
